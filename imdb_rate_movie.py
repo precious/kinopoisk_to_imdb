4ec762b904afdd1d25d26a376a8da83f6d512f76
precious <precious@i.ua>
2012-01-22 20:56:57 +0200

    probably 1st working instance

---
+++ imdb_rate_movie.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import urllib2
+import urllib
+import sys
+import re
+import json
+from parser import get_between
+
+def cookie_to_str(cookie_dict):
+	res = ''
+	for key in cookie_dict:
+		res += str(key) + '=' + str(cookie_dict[key]) + '; '
+	return res.strip()
+
+
+def imdb_rate_movie(link,rate):
+	imdb_url_str = 'http://www.imdb.com'
+	path = "/ratings/_ajax/title"
+	tt = link.split('/')[-2]
+	
+	data_dict = {"tconst": tt,
+		"rating": str(rate),
+		"auth": None,
+		"tracking_tag": "title-maindetails"}
+	
+	cookie = {}
+		
+	# updaiting cookie & data
+	headers_dict = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1",
+		"Cookie": cookie_to_str(cookie),
+		"Pragma": "no-cache",
+		"Cache-Control": "no-cache"}
+	request = urllib2.Request(link,None,headers_dict)
+	response = urllib2.urlopen(request)
+	
+	for header in response.info().headers:
+		if header.startswith('Set-Cookie'):
+			pair = header.replace('Set-Cookie:','').split(';')[0].strip().split('=')
+			print pair
+			cookie[pair[0]] = pair[1]
+	data_dict["auth"] = get_between(response.read(),'data-auth="','"')
+
+	# movie ranking
+	data = urllib.urlencode(data_dict)
+	headers_dict.update({"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1",
+		"X-Requested-With": "XMLHttpRequest",
+		"Referer": link,
+		"Content-Length": str(len(data)),
+		"Cookie": cookie_to_str(cookie)})
+	
+	request = urllib2.Request(imdb_url_str + path,data,headers_dict)
+	response = urllib2.urlopen(request)
+	if not json.loads(response.read())["status"] == 200:
+		sys.exit(1)
+	
+if __name__ == "__main__":
+	if len(sys.argv) > 2: # read from command line
+		imdb_rate_movie(sys.argv[1],int(sys.argv[2]))
+
--- kinopoisk_to_imdb.sh
+++ kinopoisk_to_imdb.sh
@@ -8,7 +8,9 @@ ENV="/usr/bin/env"
 PYTHON="`which python`"
 
 wget http://www.kinopoisk.ru/level/79/user/$KINOPOISKUSERID/votes/list/export/xls/  --header='User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1' -O $XLSFILENAME
-$PYTHON parser.py $XLSFILENAME="rates.xls" > $MOVIESFILENAME
+$PYTHON parser.py $XLSFILENAME > $MOVIESFILENAME
+
+exit
 
 IFS=$'\n'
 for line in `cat $MOVIESFILENAME`; do
@@ -19,7 +21,12 @@ for line in `cat $MOVIESFILENAME`; do
 	else
 		rate="`echo $line | tr '*' '\n' | sed -n 9p`"
 		$PYTHON imdb_rate_movie.py $link $rate
+		if [ $? != 0 ] ; then
+			echo "imdb_rate_movie.py fails"
+			exit 1
+		fi
 		echo $movie "OK"
+		echo $link $rate >> rate.log
 	fi
 done
 
